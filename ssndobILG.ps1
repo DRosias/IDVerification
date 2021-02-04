9258e676292cc5d063a43f6bd93281444a617850
igneusflama <78391691+igneusflama@users.noreply.github.com>
2021-02-03 21:10:36 -0500

    Update ssndobILG.ps1
    Refactored code
    added furlough, disable checks
    added attribute checks for 3rd party EID requests
    added no Last Login error catching
    added manager information invalid message
    removed year fields

--- ssndobILG.ps1
+++ ssndobILG.ps1
@@ -6,144 +6,167 @@ do {
 
 cls
 $user = Read-Host 'Enter Login'
-$group = "GRP-LTL"
+$furloughgroup = "GRP-LTL"
 
-
+#regex to identify EID format and output as true or false
 $mvw = [regex]::Match($user,'^[A-z]{2,5}[0-9]{3,5}$').Success
+
+#function to pull mvw info from AD
+function mvwinfopull 
+{
+#pull all info from AD
+$DisplayName = get-aduser -Server "ad.mvwcorp.com" -identity $user -properties DisplayName | Select -expand DisplayName
+$zSSN = get-aduser -Server "ad.mvwcorp.com" -identity $user -Properties zSSN | Select -expand zSSN
+$zDOB = get-aduser -Server "ad.mvwcorp.com" -identity $user -Properties zDOB | Select -expand zDOB
+$otherMobile = Get-ADUser -Server "ad.mvwcorp.com" -identity $user -Properties otherMobile | Select -expand otherMobile
+$zContractorCompany = Get-ADUser -Server "ad.mvwcorp.com" -identity $user -Properties zContractorCompany | Select -expand zContractorCompany
+$zDOBfirstfour = $zDOB.SubString(0,4)
+$zDOBlastfour  =  $zDOB.SubString(4).contains("1920")
+$zDOBlastfour1  =  $zDOB.SubString(4).contains("1900")
+$member = (Get-ADGroup $furloughgroup -server "ad.mvwcorp.com" -Properties Member |  Select-Object -ExpandProperty Member)
+$memberuser = (Get-ADUser -server "ad.mvwcorp.com" -Identity $user)
+$furloughed = ($member -contains $memberuser)
+$enabled = (Get-ADUser -server "ad.mvwcorp.com" -identity $user -Properties enabled | Select-Object -Property enabled).enabled
+$checknomanager = get-aduser -Server "ad.mvwcorp.com" -identity $user -Properties Manager | Select -expand Manager
+$zHRDepartmentID = get-aduser -Server "ad.mvwcorp.com" -identity $user -Properties zHRDepartmentID | Select -expand zHRDepartmentID
+$zHRLocationCode = get-aduser -Server "ad.mvwcorp.com" -identity $user -Properties zHRLocationCode | Select -expand zHRLocationCode
+
+#pull Manager info. If no manager info, set message
+Try{
+$zManagerEID = get-aduser -Server "ad.mvwcorp.com" -identity $user -Properties zManagerEID | Select -expand zManagerEID
+$Manager = get-aduser -Server "ad.mvwcorp.com" -identity $zManagerEID -Properties DisplayName | Select -expand DisplayName
+}
+Catch{
+$zManagerEID = "Invalid Manager listed in AD"
+$Manager = "Invalid Manager listed in AD"
+}
+
+#pull logon time and make friendly. If never logged on, set message
+Try{
+$lastlogontimestamp = get-aduser -Server "ad.mvwcorp.com" -identity $user -Properties lastlogon | Select -expand lastlogon
+$lastlogonfriendly = [DateTime]::FromFileTime($lastlogontimestamp)
+}
+Catch{
+$lastlogonfriendly = "User has never logged on"
+}
+
+#display info
+write-host ""
+write-host "Legacy Company      :     MVW" 
+write-host ""
+write-host "          Name      :    " $DisplayName
+write-host "  SSN (last 4)      :    " $zSSN
+write-host ""
+write-host "   DOB (mm/dd)      :    " $zDOBfirstfour
+write-host "       Manager      :    " $Manager
+write-host "    Last Login      :    " $lastlogonfriendly
+write-host "  Other Mobile      :    " $otherMobile
+write-host "       Company      :    " $zContractorCompany
+write-host ""
+
+#display results of furlough or disable check
+If($furloughed){write-host "                          User is furloughed `r`n"}
+if(-not $enabled){write-host "                          User is disabled `r`n"}
+if($checknomanager -eq $null){write-host "                          Manager Attribute is blank `r`n"}
+if($zManagerEID -eq "Invalid Manager listed in AD"){write-host "                          zManagerEID Attribute is invalid `r`n"}
+if($zHRDepartmentID -eq $null){write-host "                          zHRDepartmentID Attribute is invalid `r`n"}
+if($zHRLocationCode -eq $null){write-host "                          zHRLocationCode Attribute is invalid `r`n"}
+}
+
+#function to pull ILG info from AD
+function ilginfopull
+{
+$DisplayName = get-aduser -Server "ilg.ad" -identity $user -ErrorAction Stop -properties DisplayName | Select -expand DisplayName
+	
+#pull all info from AD
+$employeeID = get-aduser -Server "ilg.ad" -identity $user -properties employeeID | Select -expand employeeID
+$lastlogontimestamp = get-aduser -Server "ilg.ad" -identity $user -Properties lastlogon | Select -expand lastlogon
+$lastlogonfriendly = [DateTime]::FromFileTime($lastlogontimestamp)
+$otherMobile = Get-ADUser -Server "ilg.ad" -identity $user -Properties otherMobile | Select -expand otherMobile
+$Company = Get-ADUser -Server "ilg.ad" -identity $user -Properties Company | Select -expand Company
+$ManagerFull = Get-ADUser -Server "ilg.ad" -identity $user -Properties Manager| Select -expand Manager
+$Manager = Get-ADUser -Server "ilg.ad" -identity $ManagerFull -properties DisplayName | Select -expand DisplayName
+$member = (Get-ADGroup $furloughgroup -server "ilg.ad" -Properties Member |  Select-Object -ExpandProperty Member)
+$memberuser = (Get-ADUser -server "ilg.ad" -Identity $user)
+$furloughed = ($member -contains $memberuser)
+$enabled = (Get-ADUser -server "ilg.ad" -identity $user -Properties enabled | Select-Object -Property enabled).enabled
+	
+#display info
+write-host ""
+write-host "Legacy Company      :     ILG" 
+write-host ""
+write-host "          Name      :    " $DisplayName
+write-host "   Employee ID      :    " $employeeID
+write-host ""
+write-host "       Manager      :    " $Manager
+write-host "    Last Login      :    " $lastlogonfriendly
+write-host "       Company      :    " $Company
+write-host ""
+    
+#display results of furlough or disable check
+If($furloughed){write-host "                          User is furloughed `r`n"}
+if(-not $enabled){write-host "                          User is disabled `r`n"}
+}
+
+function vriinfopull
+{
+$DisplayName = get-aduser -Server "vri.ilg.ad" -identity $user -ErrorAction Stop -properties DisplayName | Select -expand DisplayName
+		
+#pull all info from AD
+$employeeID = get-aduser -Server "vri.ilg.ad" -identity $user -properties employeeID | Select -expand employeeID
+$lastlogontimestamp = get-aduser -Server "vri.ilg.ad" -identity $user -Properties lastlogon | Select -expand lastlogon
+$lastlogonfriendly = [DateTime]::FromFileTime($lastlogontimestamp)
+$otherMobile = Get-ADUser -Server "vri.ilg.ad" -identity $user -Properties otherMobile | Select -expand otherMobile
+$Company = Get-ADUser -Server "vri.ilg.ad" -identity $user -Properties Company | Select -expand Company
+$ManagerFull = Get-ADUser -Server "vri.ilg.ad" -identity $user -Properties Manager| Select -expand Manager
+$Manager = Get-ADUser -Server "vri.ilg.ad" -identity $ManagerFull -properties DisplayName | Select -expand DisplayName
+$member = (Get-ADGroup $furloughgroup -server "vri.ilg.ad" -Properties Member |  Select-Object -ExpandProperty Member)
+$memberuser = (Get-ADUser -server "vri.ilg.ad" -Identity $user)
+$furloughed = ($member -contains $memberuser)
+$enabled = (Get-ADUser -server "vri.ilg.ad" -identity $user -Properties enabled | Select-Object -Property enabled).enabled
+
+#display info
+write-host ""
+write-host "Legacy Company      :     ILG" 
+write-host ""
+write-host "          Name      :    " $DisplayName
+write-host "   Employee ID      :    " $employeeID
+write-host ""
+write-host "       Manager      :    " $Manager
+write-host "    Last Login      :    " $lastlogonfriendly
+write-host "       Company      :    " $Company
+write-host ""
+        
+#display results of furlough or disable check
+If($furloughed){write-host "                          User is furloughed `r`n"}
+if(-not $enabled){write-host "                          User is disabled `r`n"}
+}
+
+
+
+
+#main run
 if ($mvw){
-#old mvw hardpointed script here
-    $DisplayName = get-aduser -Server "ad.mvwcorp.com" -identity $user -properties DisplayName | Select -expand DisplayName
-    $zSSN = get-aduser -Server "ad.mvwcorp.com" -identity $user -Properties zSSN | Select -expand zSSN
-    $zDOB = get-aduser -Server "ad.mvwcorp.com" -identity $user -Properties zDOB | Select -expand zDOB
-    $zManagerEID = get-aduser -Server "ad.mvwcorp.com" -identity $user -Properties zManagerEID | Select -expand zManagerEID
-    $Manager = get-aduser -Server "ad.mvwcorp.com" -identity $zManagerEID -Properties DisplayName | Select -expand DisplayName
-    $lastlogontimestamp = get-aduser -Server "ad.mvwcorp.com" -identity $user -Properties lastlogon | Select -expand lastlogon
-    $lastlogonfriendly = [DateTime]::FromFileTime($lastlogontimestamp)
-    $otherMobile = Get-ADUser -Server "ad.mvwcorp.com" -identity $user -Properties otherMobile | Select -expand otherMobile
-    $zContractorCompany = Get-ADUser -Server "ad.mvwcorp.com" -identity $user -Properties zContractorCompany | Select -expand zContractorCompany
-    $zDOBfirstfour = $zDOB.SubString(0,4)
-    $zDOBlastfour  =  $zDOB.SubString(4).contains("1920")
-    $zDOBlastfour1  =  $zDOB.SubString(4).contains("1900")
-    $member = (Get-ADGroup $group -server "ad.mvwcorp.com" -Properties Member |  Select-Object -ExpandProperty Member)
-    $memberuser = (Get-ADUser -server "ad.mvwcorp.com" -Identity $user)
-    $furloughed = ($member -contains $memberuser)
+    mvwinfopull
+    }
 
-
-    write-host ""
-	write-host "Legacy Company      :     MVW" 
-	write-host ""
-    write-host "          Name      :    " $DisplayName
-    write-host "  SSN (last 4)      :    " $zSSN
-    write-host ""
-    write-host "   DOB (mm/dd)      :    " $zDOBfirstfour
-    write-host "    Year 1920       :    " $zDOBlastfour
-    write-host "    Year 1900       :    " $zDOBlastfour1
-    write-host "       Manager      :    " $Manager
-    write-host "    Last Login      :    " $lastlogonfriendly
-    write-host "  Other Mobile      :    " $otherMobile
-    write-host "       Company      :    " $zContractorCompany
-    write-host ""
-    If($furloughed){write-host "                          User is furloughed `r`n"}
-	}
 else{
-	#ilg script starts here
-
-    #try all except mvw exception
-	Try{
-	$DisplayName = get-aduser -Server "ilg.ad" -identity $user -ErrorAction Stop -properties DisplayName | Select -expand DisplayName
-	
-	$employeeID = get-aduser -Server "ilg.ad" -identity $user -properties employeeID | Select -expand employeeID
-	$lastlogontimestamp = get-aduser -Server "ilg.ad" -identity $user -Properties lastlogon | Select -expand lastlogon
-	$lastlogonfriendly = [DateTime]::FromFileTime($lastlogontimestamp)
-	$otherMobile = Get-ADUser -Server "ilg.ad" -identity $user -Properties otherMobile | Select -expand otherMobile
-	$Company = Get-ADUser -Server "ilg.ad" -identity $user -Properties Company | Select -expand Company
-	$ManagerFull = Get-ADUser -Server "ilg.ad" -identity $user -Properties Manager| Select -expand Manager
-	$Manager = Get-ADUser -Server "ilg.ad" -identity $ManagerFull -properties DisplayName | Select -expand DisplayName
-	$member = (Get-ADGroup $group -server "ilg.ad" -Properties Member |  Select-Object -ExpandProperty Member)
-    $memberuser = (Get-ADUser -server "ilg.ad" -Identity $user)
-    $furloughed = ($member -contains $memberuser)
-
-	
-	write-host ""
-	write-host "Legacy Company      :     ILG" 
-	write-host ""
-	write-host "          Name      :    " $DisplayName
-	write-host "   Employee ID      :    " $employeeID
-	write-host ""
-	write-host "       Manager      :    " $Manager
-	write-host "    Last Login      :    " $lastlogonfriendly
-	write-host "       Company      :    " $Company
-	write-host ""
-    If($furloughed){write-host "                          User is furloughed `r`n"}
+	try{
+    ilginfopull
 	}
 	
 	catch{
-	#check if vri
+	#try to check if user is vri.ilg.ad. If not, catch to next domain.
 		Try{
-		$DisplayName = get-aduser -Server "vri.ilg.ad" -identity $user -ErrorAction Stop -properties DisplayName | Select -expand DisplayName
-		
-		$employeeID = get-aduser -Server "vri.ilg.ad" -identity $user -properties employeeID | Select -expand employeeID
-		$lastlogontimestamp = get-aduser -Server "vri.ilg.ad" -identity $user -Properties lastlogon | Select -expand lastlogon
-		$lastlogonfriendly = [DateTime]::FromFileTime($lastlogontimestamp)
-		$otherMobile = Get-ADUser -Server "vri.ilg.ad" -identity $user -Properties otherMobile | Select -expand otherMobile
-		$Company = Get-ADUser -Server "vri.ilg.ad" -identity $user -Properties Company | Select -expand Company
-		$ManagerFull = Get-ADUser -Server "vri.ilg.ad" -identity $user -Properties Manager| Select -expand Manager
-		$Manager = Get-ADUser -Server "vri.ilg.ad" -identity $ManagerFull -properties DisplayName | Select -expand DisplayName
-	    $member = (Get-ADGroup $group -server "vri.ilg.ad" -Properties Member |  Select-Object -ExpandProperty Member)
-        $memberuser = (Get-ADUser -server "vri.ilg.ad" -Identity $user)
-        $furloughed = ($member -contains $memberuser)
-
-	
-		write-host ""
-		write-host "Legacy Company      :     ILG" 
-		write-host ""
-		write-host "          Name      :    " $DisplayName
-		write-host "   Employee ID      :    " $employeeID
-		write-host ""
-		write-host "       Manager      :    " $Manager
-		write-host "    Last Login      :    " $lastlogonfriendly
-		write-host "       Company      :    " $Company
-		write-host ""
-        If($furloughed){write-host "                          User is furloughed `r`n"}
+		vriinfopull
 		}
 		
-		#not ilg or vri
+		#if no other domain, assume it's a strangely formatted EID and default back to mvw.
 		Catch{
-			cls
+			#cls and rewrite input for a consistent display
+            cls
 			write-host "Enter Login:" $user
-			#old mvw hardpointed script here
-			$DisplayName = get-aduser -Server "ad.mvwcorp.com" -identity $user -properties 	DisplayName | Select -expand DisplayName
-			$zSSN = get-aduser -Server "ad.mvwcorp.com" -identity $user -Properties zSSN | Select -expand zSSN
-			$zDOB = get-aduser -Server "ad.mvwcorp.com" -identity $user -Properties zDOB | Select -expand zDOB
-			$zManagerEID = get-aduser -Server "ad.mvwcorp.com" -identity $user -Properties zManagerEID | Select -expand zManagerEID
-			$Manager = get-aduser -Server "ad.mvwcorp.com" -identity $zManagerEID -Properties DisplayName | Select -expand DisplayName
-			$lastlogontimestamp = get-aduser -Server "ad.mvwcorp.com" -identity $user -Properties lastlogon | Select -expand lastlogon
-			$lastlogonfriendly = [DateTime]::FromFileTime($lastlogontimestamp)
-			$otherMobile = Get-ADUser -Server "ad.mvwcorp.com" -identity $user -Properties otherMobile | Select -expand otherMobile
-			$zContractorCompany = Get-ADUser -Server "ad.mvwcorp.com" -identity $user -Properties zContractorCompany | Select -expand zContractorCompany
-			$zDOBfirstfour = $zDOB.SubString(0,4)
-			$zDOBlastfour  =  $zDOB.SubString(4).contains("1920")
-			$zDOBlastfour1  =  $zDOB.SubString(4).contains("1900")
-            $member = (Get-ADGroup $group -server "ad.mvwcorp.com" -Properties Member |  Select-Object -ExpandProperty Member)
-            $memberuser = (Get-ADUser -server "ad.mvwcorp.com" -Identity $user)
-            $furloughed = ($member -contains $memberuser)
-
-		
-			write-host ""
-			write-host "Legacy Company      :     MVW" 
-			write-host ""
-			write-host "          Name      :    " $DisplayName
-			write-host "  SSN (last 4)      :    " $zSSN
-			write-host ""
-			write-host "   DOB (mm/dd)      :    " $zDOBfirstfour
-			write-host "    Year 1920       :    " $zDOBlastfour
-			write-host "    Year 1900       :    " $zDOBlastfour1
-			write-host "       Manager      :    " $Manager
-			write-host "    Last Login      :    " $lastlogonfriendly
-			write-host "  Other Mobile      :    " $otherMobile
-			write-host "       Company      :    " $zContractorCompany
-			write-host ""
-            If($furloughed){write-host "                          User is furloughed `r`n"}
+			mvwinfopull
 		}
     }
 
@@ -161,11 +184,13 @@ $pause = Read-Host 'Press enter to lookup another EID'
     $zDOBlastfour = ""
     $zDOBlastfour1=""
     $ManagerFull=""
-	$Manager = ""
+    $Manager = ""
     $lastlogon = ""
     $member = ""
     $memberuser = ""
     $furloughed = ""
+    $enabled = ""
+    $checknomanager = ""
 
 
 }
